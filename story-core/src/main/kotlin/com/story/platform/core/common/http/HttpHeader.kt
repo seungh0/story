066dd24a50873e3d7fe29cbad580bf5a135d03b4
seungh0 <will.seungho@gmail.com>
2023-10-15 03:31:20 +0900

    헤더 설명 변경

--- story-core/src/main/kotlin/com/story/platform/core/common/http/HttpHeader.kt
+++ story-core/src/main/kotlin/com/story/platform/core/common/http/HttpHeader.kt
@@ -6,11 +6,20 @@ enum class HttpHeader(
     val example: String,
 ) {
 
-    X_STORY_API_KEY(header = "X-Story-Api-Key", description = "Authentication Key", "authentication-key"),
-    X_REQUEST_ID(header = "X-Request-Id", description = "Request-Id", "request-id"),
+    X_STORY_API_KEY(
+        header = "X-Story-Api-Key",
+        description = "Story Platform 인증 키",
+        example = "a6d63e0e-7e5c-4fd8-85f2-fc9eb36fa9bd"
+    ),
+    X_STORY_REQUEST_ACCOUNT_ID(
+        header = "X-Story-Request-Account-Id",
+        description = "요청자의 계정 ID",
+        "ac6e589e-5411-43c0-a166-d3fe068db77c"
+    ),
+    X_STORY_NONCE("X-Story-Nonce", description = "논스 토큰 (About Nonce 참고)", "92a41af5-1b70-48db-9948-99b114bce9ee"),
+
+    X_REQUEST_ID(header = "X-Request-Id", description = "Request-Id", "c64af254-d067-475c-b4ce-15a6f0a73f99"),
     X_FORWARDED_FOR(header = "X-Forwarded-For", description = "X-Forwarded-For", "127.0.0.1"),
-    X_STORY_REQUEST_ACCOUNT_ID(header = "X-Story-Request-Account-Id", description = "Request-Account-Id", "account-Id"),
-    X_STORY_NONCE("X-Story-Nonce", description = "Nonce Key", "nonce-key"),
-    ACCEPT_LANGUAGE("Accept-Language", description = "Accept-Language", "ko/en"),
+    ACCEPT_LANGUAGE("Accept-Language", description = "Accept-Language", example = "ko/en"),
 
 }
