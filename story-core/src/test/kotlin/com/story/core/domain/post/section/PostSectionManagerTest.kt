006bbd4f132be46a093af594d5e15a7c3d26342d
seungh0 <will.seungho@gmail.com>
2024-02-25 00:04:16 +0900

    LinkPostSection 추가

--- story-core/src/test/kotlin/com/story/core/domain/post/section/PostSectionManagerTest.kt
+++ story-core/src/test/kotlin/com/story/core/domain/post/section/PostSectionManagerTest.kt
@@ -1,97 +1,76 @@
-// package com.story.core.domain.post.section
-//
-// import com.story.core.common.json.toJson
-// import com.story.core.domain.file.FileType
-// import com.story.core.domain.post.PostSpaceKey
-// import com.story.core.domain.post.section.image.ImagePostSectionContent
-// import com.story.core.domain.post.section.image.ImagePostSectionContentRequest
-// import com.story.core.domain.post.section.image.ImagePostSectionHandler
-// import com.story.core.domain.post.section.text.TextPostSectionContent
-// import com.story.core.domain.post.section.text.TextPostSectionContentRequest
-// import com.story.core.domain.post.section.text.TextPostSectionHandler
-// import com.story.core.infrastructure.file.FileProperties
-// import io.kotest.core.spec.style.StringSpec
-// import io.kotest.matchers.collections.shouldHaveSize
-// import io.kotest.matchers.shouldBe
-//
-// class PostSectionManagerTest : StringSpec({
-//
-//    val manager = PostSectionManager(
-//        postSectionHandlerFinder = { sectionType ->
-//            when (sectionType) {
-//                PostSectionType.TEXT -> TextPostSectionHandler()
-//                PostSectionType.IMAGE -> ImagePostSectionHandler(
-//                    properties = FileProperties(
-//                        properties = mapOf(
-//                            FileType.IMAGE to FileProperties(
-//                                domain = "https://cdn.story.com"
-//                            )
-//                        )
-//                    )
-//                )
-//
-//            }
-//        }
-//    )
-//
-//    "포스트 섹션을 생성합니다" {
-//        // given
-//        val request1 = TextPostSectionContentRequest(
-//            content = "토키에요",
-//            priority = 1,
-//        )
-//        val request2 = TextPostSectionContentRequest(
-//            content = "뽀미에요",
-//            priority = 2,
-//        )
-//
-//        val request3 = ImagePostSectionContentRequest(
-//            priority = 3,
-//            path = "/pictures",
-//            fileName = "flower.png",
-//        )
-//
-//        // when
-//        val sut = manager.makePostSections(
-//            requests = listOf(request1, request2, request3),
-//            postSpaceKey = PostSpaceKey(
-//                workspaceId = "story",
-//                componentId = "user-post",
-//                spaceId = "tokki",
-//            ),
-//            ownerId = "tokki",
-//            postId = 1000L,
-//            parentId = null,
-//        )
-//
-//        // then
-//        sut shouldHaveSize 3
-//        sut.forEach { section ->
-//            section.key.workspaceId shouldBe "story"
-//            section.key.componentId shouldBe "user-post"
-//            section.key.spaceId shouldBe "tokki"
-//            section.key.parentId shouldBe ""
-//            section.key.slotId shouldBe 10L
-//            section.key.postId shouldBe 1000L
-//        }
-//
-//        sut[0].sectionType shouldBe PostSectionType.TEXT
-//        sut[0].key.priority shouldBe 1L
-//        sut[0].data shouldBe TextPostSectionContent(content = "토키에요").toJson()
-//
-//        sut[1].sectionType shouldBe PostSectionType.TEXT
-//        sut[1].key.priority shouldBe 2L
-//        sut[1].data shouldBe TextPostSectionContent(content = "뽀미에요").toJson()
-//
-//        sut[2].sectionType shouldBe PostSectionType.IMAGE
-//        sut[2].key.priority shouldBe 3L
-//        sut[2].data shouldBe ImagePostSectionContent(
-//            path = "/pictures",
-//            width = 0,
-//            height = 0,
-//            fileSize = 0,
-//            fileName = "flower.png",
-//        ).toJson()
-//    }
-//
-// })
+package com.story.core.domain.post.section
+
+import com.story.core.IntegrationTest
+import com.story.core.common.json.toJson
+import com.story.core.domain.post.PostSpaceKey
+import com.story.core.domain.post.section.link.LinkPostSectionContent
+import com.story.core.domain.post.section.link.LinkPostSectionContentRequest
+import com.story.core.domain.post.section.text.TextPostSectionContent
+import com.story.core.domain.post.section.text.TextPostSectionContentRequest
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.collections.shouldHaveSize
+import io.kotest.matchers.shouldBe
+
+@IntegrationTest
+class PostSectionManagerTest(
+    private val postSectionManager: PostSectionManager,
+) : StringSpec({
+
+    "포스트 섹션을 생성합니다" {
+        // given
+        val request1 = TextPostSectionContentRequest(
+            content = "토키에요",
+            priority = 1,
+        )
+        val request2 = TextPostSectionContentRequest(
+            content = "뽀미에요",
+            priority = 2,
+        )
+
+        val request3 = LinkPostSectionContentRequest(
+            priority = 3,
+            link = "https://google.com",
+            extra = emptyMap(),
+        )
+
+        // when
+        val sut = postSectionManager.makePostSections(
+            requests = listOf(request1, request2, request3),
+            postSpaceKey = PostSpaceKey(
+                workspaceId = "story",
+                componentId = "user-post",
+                spaceId = "tokki",
+            ),
+            ownerId = "tokki",
+            postId = 1000L,
+            parentId = null,
+        )
+
+        // then
+        sut shouldHaveSize 3
+        sut.forEach { section ->
+            section.key.workspaceId shouldBe "story"
+            section.key.componentId shouldBe "user-post"
+            section.key.spaceId shouldBe "tokki"
+            section.key.parentId shouldBe ""
+            section.key.slotId shouldBe 10L
+            section.key.postId shouldBe 1000L
+        }
+
+        sut[0].sectionType shouldBe PostSectionType.TEXT
+        sut[0].key.priority shouldBe 1L
+        sut[0].data shouldBe TextPostSectionContent(content = "토키에요", extra = emptyMap()).toJson()
+
+        sut[1].sectionType shouldBe PostSectionType.TEXT
+        sut[1].key.priority shouldBe 2L
+        sut[1].data shouldBe TextPostSectionContent(content = "뽀미에요", extra = emptyMap()).toJson()
+
+        sut[2].sectionType shouldBe PostSectionType.LINK
+        sut[2].key.priority shouldBe 3L
+        sut[2].data shouldBe LinkPostSectionContent(
+            link = "https://google.com",
+            extra = emptyMap(),
+        ).toJson()
+    }
+
+})
--- story-core/src/test/kotlin/com/story/core/domain/post/section/image/ImagePostSectionHandlerTest.kt
+++ story-core/src/test/kotlin/com/story/core/domain/post/section/image/ImagePostSectionHandlerTest.kt
@@ -1,97 +1,137 @@
-// package com.story.core.domain.post.section.image
-//
-// import com.story.core.domain.file.FileType
-// import com.story.core.infrastructure.file.FileProperties
-// import io.kotest.core.spec.style.StringSpec
-// import io.kotest.matchers.maps.shouldContainExactly
-// import io.kotest.matchers.maps.shouldHaveSize
-//
-// class ImagePostSectionHandlerTest : StringSpec({
-//
-//    val handler = ImagePostSectionHandler(
-//        properties = FileProperties(
-//            properties = mapOf(
-//                FileType.IMAGE to FileProperties(
-//                    domain = "https://cdn.story.com"
-//                )
-//            ),
-//        )
-//    )
-//
-//    "ImagePostSectionContentRequest -> ImagePostSectionContent" {
-//        // given
-//        val section1 = ImagePostSectionContentRequest(
-//            priority = 1,
-//            path = "/pictures",
-//            fileName = "flower.png",
-//        )
-//        val section2 = ImagePostSectionContentRequest(
-//            priority = 2,
-//            path = "/pictures",
-//            fileName = "dog.png",
-//        )
-//
-//        // when
-//        val sut = handler.makeContents(listOf(section1, section2))
-//
-//        // then
-//        sut shouldHaveSize 2
-//        sut shouldContainExactly mapOf(
-//            section1 to ImagePostSectionContent(
-//                path = "/pictures",
-//                width = 0,
-//                height = 0,
-//                fileSize = 0,
-//                fileName = "flower.png",
-//            ),
-//            section2 to ImagePostSectionContent(
-//                path = "/pictures",
-//                width = 0,
-//                height = 0,
-//                fileSize = 0,
-//                fileName = "dog.png",
-//            )
-//        )
-//    }
-//
-//    "ImagePostSectionContent -> ImagePostSectionContentResponse" {
-//        // given
-//        val content1 = ImagePostSectionContent(
-//            path = "/pictures",
-//            width = 120,
-//            height = 86,
-//            fileSize = 1024,
-//            fileName = "flower.png",
-//        )
-//        val content2 = ImagePostSectionContent(
-//            path = "/pictures",
-//            width = 120,
-//            height = 86,
-//            fileSize = 1024,
-//            fileName = "dog.png",
-//        )
-//
-//        // when
-//        val sut = handler.makeContentResponse(listOf(content1, content2))
-//
-//        // then
-//        sut shouldHaveSize 2
-//        sut shouldContainExactly mapOf(
-//            content1 to ImagePostSectionContentResponse(
-//                path = "/pictures/flower.png",
-//                width = 120,
-//                height = 86,
-//                fileSize = 1024,
-//                domain = "https://cdn.story.com",
-//            ),
-//            content2 to ImagePostSectionContentResponse(
-//                path = "/pictures/dog.png",
-//                width = 120,
-//                height = 86,
-//                fileSize = 1024,
-//                domain = "https://cdn.story.com",
-//            )
-//        )
-//    }
-//
-// })
+package com.story.core.domain.post.section.image
+
+import com.story.core.domain.file.FileResponse
+import com.story.core.domain.file.FileRetriever
+import com.story.core.domain.file.FileType
+import com.story.core.infrastructure.file.FileProperties
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.maps.shouldContainExactly
+import io.kotest.matchers.maps.shouldHaveSize
+import io.mockk.coEvery
+import io.mockk.mockk
+
+class ImagePostSectionHandlerTest : StringSpec({
+
+    val fileRetriever = mockk<FileRetriever>()
+
+    val handler = ImagePostSectionHandler(
+        properties = FileProperties(
+            properties = mapOf(
+                FileType.IMAGE to FileProperties.Properties(
+                    domain = "https://cdn.story.com"
+                )
+            )
+        ),
+        fileRetriever = fileRetriever,
+    )
+
+    "ImagePostSectionContentRequest -> ImagePostSectionContent" {
+        // given
+        val section1 = ImagePostSectionContentRequest(
+            priority = 1,
+            fileId = 1L,
+            extra = mapOf(
+                "extra" to 10
+            ),
+        )
+        val section2 = ImagePostSectionContentRequest(
+            priority = 2,
+            fileId = 2L,
+            extra = emptyMap(),
+        )
+
+        coEvery { fileRetriever.getFiles(any(), any(), any()) } returns mapOf(
+            1L to FileResponse(
+                path = "/pictures",
+                width = 40,
+                height = 20,
+                fileSize = 100,
+                fileName = "flower.png",
+                domain = "https://cdn.story.com",
+                fileId = 1L,
+            ),
+            2L to FileResponse(
+                path = "/pictures",
+                width = 80,
+                height = 60,
+                fileSize = 1000,
+                fileName = "dog.png",
+                domain = "https://cdn.story.com",
+                fileId = 2L,
+            )
+        )
+
+        // when
+        val sut = handler.makeContents(workspaceId = "story", requests = listOf(section1, section2))
+
+        // then
+        sut shouldHaveSize 2
+        sut shouldContainExactly mapOf(
+            section1 to ImagePostSectionContent(
+                path = "/pictures",
+                width = 40,
+                height = 20,
+                fileSize = 100,
+                fileName = "flower.png",
+                extra = mapOf(
+                    "extra" to 10
+                ),
+            ),
+            section2 to ImagePostSectionContent(
+                path = "/pictures",
+                width = 80,
+                height = 60,
+                fileSize = 1000,
+                fileName = "dog.png",
+                extra = emptyMap(),
+            )
+        )
+    }
+
+    "ImagePostSectionContent -> ImagePostSectionContentResponse" {
+        // given
+        val content1 = ImagePostSectionContent(
+            path = "/pictures",
+            width = 120,
+            height = 86,
+            fileSize = 1024,
+            fileName = "flower.png",
+            extra = emptyMap(),
+        )
+        val content2 = ImagePostSectionContent(
+            path = "/pictures",
+            width = 120,
+            height = 86,
+            fileSize = 1024,
+            fileName = "dog.png",
+            extra = mapOf(
+                "extra" to 10
+            ),
+        )
+
+        // when
+        val sut = handler.makeContentResponse(listOf(content1, content2))
+
+        // then
+        sut shouldHaveSize 2
+        sut shouldContainExactly mapOf(
+            content1 to ImagePostSectionContentResponse(
+                path = "/pictures/flower.png",
+                width = 120,
+                height = 86,
+                domain = "https://cdn.story.com",
+                extra = emptyMap(),
+            ),
+            content2 to ImagePostSectionContentResponse(
+                path = "/pictures/dog.png",
+                width = 120,
+                height = 86,
+                domain = "https://cdn.story.com",
+                extra = mapOf(
+                    "extra" to 10
+                ),
+            )
+        )
+    }
+
+})
